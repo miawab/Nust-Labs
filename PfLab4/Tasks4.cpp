64c49d8982344be7d834db093b944d3688674d7f
Ibrahim Awab <179867549+miawab@users.noreply.github.com>
2025-10-03 12:15:29 +0500

    Task5

--- PfLab4/Tasks4.cpp
+++ PfLab4/Tasks4.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int length, width, area, perimeter;
-    cout<<"enter length and width: ";
-    cin>>length>>width;
-    area = length*width;
-    perimeter = 2*(length + width) ;
-    if (area>length) {
-        cout<<"area > length"<<endl;
-    } else {
-        cout<<"area < length"<<endl;
-    }
+    int num,d1,d2,d4,d5;
+    bool check;
+    cout<<"enter a 5 digit number: ";
+    cin>>num;
+    d5 = num%10;
+    d4 = (num/10)%10;
+    d2 = (num/1000)%10;
+    d1 = (num/10000)%10;
+    cout<<num<<" is ";
+    cout<<(d5 == d1 && d4 == d2? "a palindrome": "not a palindrome")<<endl;
     return 0;
 }
