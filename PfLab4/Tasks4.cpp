da7ea24c65910a4536e2a693b64ccd00f4d513e9
Ibrahim Awab <179867549+miawab@users.noreply.github.com>
2025-10-03 12:30:40 +0500

    Task6

--- PfLab4/Tasks4.cpp
+++ PfLab4/Tasks4.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main() {
-    int num,d1,d2,d4,d5;
-    bool check;
-    cout<<"enter a 5 digit number: ";
-    cin>>num;
-    d5 = num%10;
-    d4 = (num/10)%10;
-    d2 = (num/1000)%10;
-    d1 = (num/10000)%10;
-    cout<<num<<" is ";
-    cout<<(d5 == d1 && d4 == d2? "a palindrome": "not a palindrome")<<endl;
+    char input;
+    cout<<"enter a character: ";
+    cin>>input;
+    if (input >=65 && input <=90){
+        cout<<"is a capital letter"<<endl;
+    } else if (input >=97 && input <=122) {
+        cout<<"is a lower case letter"<<endl;
+    } else if (input >=48 && input <=57){
+        cout<<"is a number"<<endl;
+    } else {
+        cout<<"is a special character"<<endl;
+    }
     return 0;
 }
