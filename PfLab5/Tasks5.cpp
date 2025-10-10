042437d5198b0e91e451fa43dc2eb3d8d467f532
Ibrahim Awab <179867549+miawab@users.noreply.github.com>
2025-10-10 12:13:34 +0500

    Task 3

--- PfLab5/Tasks5.cpp
+++ PfLab5/Tasks5.cpp
@@ -1,72 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int lines=10;
-    int i=1;
-    int x=0;
-    while (i<=lines) {
-        x=0;
-        while (x<i) {
-            cout<<"*";
-            x++;
+    long long int num;
+    cout<<"enter a number: ";
+    cin>>num;
+    while(num!=0) { 
+        if(num%10!=0) {
+            cout<<(num%10);
         }
-        cout<<endl;
-        i++;
+        else {
+            cout<<0;
+        }
+        num=num/10;
     }
-
     cout<<endl;
-    i=10;
-        while (i>=0) {
-        x=0;
-        while (x<i) {
-            cout<<"*";
-            x++;
-        }
-        cout<<endl;
-        i--;
-    }
-
-
-    cout<<endl;
-    lines=10;
-    i=1;
-    x=0;
-    while (i<=lines) {
-        x=0;
-        while (x<i-1) {
-            cout<<" ";
-            x++;
-        }
-        x=0;
-        while(x<lines-i+1){
-            cout<<"*";
-            x++;
-        }
-        cout<<endl;
-        i++;
-
-    }
-
-    cout<<endl;
-    lines=10;
-    i=1;
-    x=0;
-    while (i<=lines) { 
-        x=0;
-        while(x<lines-i){
-            cout<<" ";
-            x++;
-        }
-        x=0;
-        while (x<i) {
-            cout<<"*";
-            x++;
-        }
-       
-        cout<<endl;
-        i++;
-
-    }
-
     return 0;
 }
