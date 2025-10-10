0a79bfbcdaa69a2cd063fb96a0df9e32bbe405c9
Ibrahim Awab <179867549+miawab@users.noreply.github.com>
2025-10-10 12:45:40 +0500

    Task 5

--- PfLab5/Tasks5.cpp
+++ PfLab5/Tasks5.cpp
@@ -1,35 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int num,num2, sum, original, d,dnum,y;
-    cout<<"enter a number: ";
-    cin>>num;
-    original=num;
-    sum=0;
-    num2= num;
-    dnum=0;
-    y=0;
-    while(num2!=0) {
-        num2= num2/10;
-        dnum++;
-    }
-    num2=1;
-    while(num!=0) {
-        y=0;
-        d=num%10;
-        num2=1;
-        while(y<dnum) {
-            num2=num2*d;
-            y++;
-        }
-        sum= sum+(num2);
-        num/=10;
-    }
-    if (original == sum) {
-        cout<<"armstrong";
-    } else {
-        cout<<"no";
-    }
-    cout<<endl;
+    int y;
+    do {
+        cout<<endl;
+        cout<<"Choose an option\n"<<endl;
+        cout<<"1.Add\n2.Subrtract\n3.Multiply\n0.Exit"<<endl;
+        cout<<endl<<"option: ";
+        cin>>y;
+    } while (y!=0);
+    
     return 0;
 }
