944c84703feddc448f3b68d42caf9b53a8a3d43c
Ibrahim Awab <179867549+miawab@users.noreply.github.com>
2025-10-11 12:04:43 +0500

    fixed task 1

--- PfA1/geo_tracker_basic.cpp
+++ PfA1/geo_tracker_basic.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 int main() {
     string name;
-    int lon, lat;
-    cout<<"enter name: ";
+    float lon, lat;
+    float Llon, Llat;
+    Llon= 31.233334;
+    Llat = 30.033333;
+    cout<<"enter first name: ";
     cin>>name;
     cout<<"enter longitude and latitude respectively: ";
     cin>>lon>>lat;
-    
+    cout<<endl;
+
+    cout<<name<<endl;
+    cout<<"* Landmark is at latitude "<<Llat<<" and longitude "<<Llon<<endl;
+    cout<<"* Current Latitude is "<<lat<<" and longitude is "<<lon<<endl<<endl;
     return 0;
 }
