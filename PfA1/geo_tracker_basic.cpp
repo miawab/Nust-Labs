90aed529df2c6d148299902121cb7520a757e8f3
Ibrahim Awab <179867549+miawab@users.noreply.github.com>
2025-10-11 11:14:34 +0500

    Task 1 setting up variables.

---
+++ PfA1/geo_tracker_basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    int lon, lat;
+    cout<<"enter name: ";
+    cin>>name;
+    cout<<"enter longitude and latitude respectively: ";
+    cin>>lon>>lat;
+    
+    return 0;
+}
