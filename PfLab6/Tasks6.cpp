bb18519086393ad8e36395d1249bb156ff70ddd6
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-10-19 21:51:03 +0500

    Task 5

--- PfLab6/Tasks6.cpp
+++ PfLab6/Tasks6.cpp
@@ -1,26 +1,67 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int n, i, j;
-    cout<<"enter number of rows: ";
+    int d,n,num,temp, power;
+
+    cout<<"input: ";
     cin>>n;
+    num = n;
+    cout<<"output: ";
 
-    for (i=0;i<n;i++) {
-
-        for (j=n-i;j>1;j--) {
-            cout<<" ";
-        }
-
-        for (j=0;j<i+1;j++) {
-            cout<<j+1;
-        }
-
-        for (j=i;j>0;j--) {
-            cout<<j;
-        }
-        cout<<endl;
+    for(d=0;n>0;d++) {
+        n /= 10;
     }
+
+    for(;d>0;d--) {
+
+        temp = num/10 ;
+        power = pow(10,d-1);
+        switch ((num/power)%10) {
+            case 0:
+            cout<<"zero, ";
+            break;
+
+            case 1: 
+            cout<<"one, ";
+            break;
+
+            case 2: 
+            cout<<"two, ";
+            break;
+
+            case 3: 
+            cout<<"three, ";
+            break;
+
+            case 4: 
+            cout<<"four, ";
+            break;
+
+            case 5: 
+            cout<<"five, ";
+            break;
+
+            case 6: 
+            cout<<"six, ";
+            break;
+
+            case 7: 
+            cout<<"seven, ";
+            break;
+
+            case 8: 
+            cout<<"eight, ";
+            break;
+
+            case 9: 
+            cout<<"nine, ";
+            break;
+        }
+
+    }
+    cout<<endl;
 
     return 0;
 }
