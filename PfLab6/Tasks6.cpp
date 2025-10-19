4fa84fa91a772a5bba2d2c8a38381a6ca126050c
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-10-19 22:08:18 +0500

    Task 6

--- PfLab6/Tasks6.cpp
+++ PfLab6/Tasks6.cpp
@@ -3,65 +3,15 @@
 using namespace std;
 
 int main() {
-    int d,n,num,temp, power;
-
-    cout<<"input: ";
+    int n, i ,prod;
+    cout<<"enter a number: ";
     cin>>n;
-    num = n;
-    cout<<"output: ";
+    for(i=0;i<10;i++) {
 
-    for(d=0;n>0;d++) {
-        n /= 10;
-    }
+        prod = n*(i+1);
+        cout<<n<<" x "<<i+1<<" = "<<prod<<endl;
 
-    for(;d>0;d--) {
-
-        temp = num/10 ;
-        power = pow(10,d-1);
-        switch ((num/power)%10) {
-            case 0:
-            cout<<"zero, ";
-            break;
-
-            case 1: 
-            cout<<"one, ";
-            break;
-
-            case 2: 
-            cout<<"two, ";
-            break;
-
-            case 3: 
-            cout<<"three, ";
-            break;
-
-            case 4: 
-            cout<<"four, ";
-            break;
-
-            case 5: 
-            cout<<"five, ";
-            break;
-
-            case 6: 
-            cout<<"six, ";
-            break;
-
-            case 7: 
-            cout<<"seven, ";
-            break;
-
-            case 8: 
-            cout<<"eight, ";
-            break;
-
-            case 9: 
-            cout<<"nine, ";
-            break;
         }
-
-    }
-    cout<<endl;
 
     return 0;
 }
