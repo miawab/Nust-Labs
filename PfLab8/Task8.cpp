afbe39e2ad8e1f286ce0583b64f2133549f7e859
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-02 21:53:26 +0500

    Task 4

--- PfLab8/Task8.cpp
+++ PfLab8/Task8.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int multiply(int a=1,int b=1,int c=1,int d=1,int e=1) {
-    return a*b*c*d*e;
+int square(int a) {
+    return a*a;
+}
+int cube (int a) {
+    return a*a*a;
 }
 
 int main() {
-    int num,a,b,c,d,e;
-    cout<<"enter the amount of numbers to mulitply (2-5): ";
-    cin>>num;
-
-    cout<<"enter your numbers: ";
-    switch (num){
-        case 2:
-            cin>>a>>b;
-            cout<<"ANS: "<<multiply(a,b)<<endl;
-            break;
-        case 3:
-            cin>>a>>b>>c;
-            cout<<"ANS: "<<multiply(a,b,c)<<endl;
-            break;
-        case 4:
-            cin>>a>>b>>c>>d;
-            cout<<"ANS: "<<multiply(a,b,c,d)<<endl;
-            break;
-        case 5:
-            cin>>a>>b>>c>>d>>e;
-            cout<<"ANS: "<<multiply(a,b,c,d,e)<<endl;
-            break;
-        default:
-            cout<<"ERROR! run the command again"<<endl;
-
-    }
-
-
-    return 0;
+    int a;
+    cout<<"enter a number: ";
+    cin>>a;
+    cout<<"square of the number= "<<square(a)<<endl;
+    cout<<"cube of the number= "<<cube(a)<<endl;
 }
