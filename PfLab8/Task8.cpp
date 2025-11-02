729c26a7b78d063a36c3aa981fa6b223140395bf
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-02 21:38:49 +0500

    Task 3

--- PfLab8/Task8.cpp
+++ PfLab8/Task8.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+using namespace std;
 
-void calculateSquare(int &num);
-
-int main() {
-    int number;
-    std::cout << "Enter a number: ";
-    std::cin >> number;
-    calculateSquare(number); 
-    std::cout << "The square of the number is: " << number << std::endl; return 0;
+int multiply(int a=1,int b=1,int c=1,int d=1,int e=1) {
+    return a*b*c*d*e;
 }
 
-void calculateSquare(int &num){
-    num = num * num;
+int main() {
+    int num,a,b,c,d,e;
+    cout<<"enter the amount of numbers to mulitply (2-5): ";
+    cin>>num;
+
+    cout<<"enter your numbers: ";
+    switch (num){
+        case 2:
+            cin>>a>>b;
+            cout<<"ANS: "<<multiply(a,b)<<endl;
+            break;
+        case 3:
+            cin>>a>>b>>c;
+            cout<<"ANS: "<<multiply(a,b,c)<<endl;
+            break;
+        case 4:
+            cin>>a>>b>>c>>d;
+            cout<<"ANS: "<<multiply(a,b,c,d)<<endl;
+            break;
+        case 5:
+            cin>>a>>b>>c>>d>>e;
+            cout<<"ANS: "<<multiply(a,b,c,d,e)<<endl;
+            break;
+        default:
+            cout<<"ERROR! run the command again"<<endl;
+
+    }
+
+
+    return 0;
 }
