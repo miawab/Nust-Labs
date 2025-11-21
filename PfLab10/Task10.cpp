25bc12b2e14a1f91dc0d0aac90e6be3cdbb9576a
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-21 19:08:21 +0500

    Task 2

--- PfLab10/Task10.cpp
+++ PfLab10/Task10.cpp
@@ -4,15 +4,24 @@ using namespace std;
 
 int main(){
     int arr1[10];
-    int sum = 0;
-    for(int i = 0; i <10; i++) {
+    int large = 0;
+
+    cout<<"enter number: ";
+    cin>>arr1[0];
+
+    for(int i = 1; i <10; i++) {
         cout<<"enter number: ";
         cin>>arr1[i];
-        sum += arr1[i];
+        if (large < arr1[i])
+            large = arr1[i];    
     }
-    int avg = sum/ 10;
-    cout<<"average is "<<avg<<endl;
-}
 
+    for (int i = 1; i<10; i++) {
+        if(arr1[i] != large)
+            continue;
+        cout<<large<<" is the largest num and "<<i<<" is it's index\n"<<endl;  
+        break;      
+    }
+}
 
 
