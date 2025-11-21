8b02fc8b9c45a6f2aead1e34ac5368ed1b97d1bf
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-22 00:11:21 +0500

    Task 4

--- PfLab10/Task10.cpp
+++ PfLab10/Task10.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    int arr1[20];
+    int arr2[20];
+    int x = 0;
 
-int main(){
-    int arr1[10];
-    int arr2[10];    
-    int arr3[10];
-    int i3 = 0;
-
-    cout<< "Enter 10 numbers for set 1: ";
-    for (int i = 0; i < 10; i++) 
+    cout << "Enter 20 numbers for array: ";
+    for (int i = 0; i < 20; i++) {
         cin >> arr1[i];
-    
-    cout << "Enter 10 numbers for set 2: ";
-    for (int i = 0; i < 10; i++) 
-        cin >> arr2[i];
+        bool isDuplicate = false;
 
-
-    for(int i =0;i<10;i++){
-        for(int x=0;x<i3+1;x++){
-            if(arr1[i]==arr3[x]){
-            i++;
-            break;
-            }
-        }
-        for(int j=0;j<10;j++){
-            if (arr1[i] == arr2[j] ){
-                arr3[i3] = arr1[i];
-                i3++;
+        for (int j = 0; j < x; j++) {
+            if (arr2[j] == arr1[i]) {
+                isDuplicate = true;
                 break;
             }
         }
+        if (!isDuplicate) {
+            arr2[x] = arr1[i];
+            x++;
+        }
     }
-
-    for(int x=0;x<i3;x++){
-        cout<<arr3[x]<<", ";
+    for (int j = 0; j < x; j++) {
+        cout << arr2[j] << ", ";
     }
     cout<<endl;
 }
-
-
