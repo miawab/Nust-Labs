e980b3e884a84528cccf0bfc88f8dcd9bd97ae86
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-28 11:40:52 +0500

    Task 3

--- PfLab11/Task11.cpp
+++ PfLab11/Task11.cpp
@@ -2,48 +2,34 @@
 using namespace std;
 
 int main(){
-    int A[3][3], B[3][3], C[3][3];
+    int arr[10];
 
-    cout << "Enter first 3x3 matrix (9 integers), row by row:\n";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cin >> A[i][j];
-        }
+    cout << "Enter 10 integers: ";
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
     }
 
-    cout << "Enter second 3x3 matrix (9 integers), row by row:\n";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cin >> B[i][j];
-        }
+    cout << "\nOriginal data:\n";
+    for(int i = 0; i < 10; i++){
+        cout << arr[i] << " ";
     }
+    cout << '\n';
 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            C[i][j] = 0;
-            for(int k=0;k<3;k++){
-                C[i][j] += A[i][k] * B[k][j];
+    for(int pass = 0; pass < 9; pass++){
+        for(int j = 0; j < 9 - pass; j++){
+            if(arr[j] < arr[j+1]){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
             }
         }
     }
 
-    cout << "\nFirst matrix:\n";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++) cout << A[i][j] << " ";
-        cout << '\n';
+    cout << "\nSorted:\n";
+    for(int i = 0; i < 10; i++){
+        cout << arr[i] << " ";
     }
-
-    cout << "\nSecond matrix:\n";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++) cout << B[i][j] << " ";
-        cout << '\n';
-    }
-
-    cout << "\nProduct matrix:\n";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++) cout << C[i][j] << " ";
-        cout << '\n';
-    }
+    cout << '\n';
 
     return 0;
 }
