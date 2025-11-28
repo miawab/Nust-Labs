b09f51b37f48ac17ebd9b42479d8c4153074fa7d
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-28 11:58:29 +0500

    Task 4

--- PfLab11/Task11.cpp
+++ PfLab11/Task11.cpp
@@ -5,31 +5,35 @@ int main(){
     int arr[10];
 
     cout << "Enter 10 integers: ";
-    for(int i = 0; i < 10; i++){
-        cin >> arr[i];
-    }
+    for(int i = 0; i < 10; i++) cin >> arr[i];
 
-    cout << "\nOriginal data:\n";
-    for(int i = 0; i < 10; i++){
-        cout << arr[i] << " ";
-    }
-    cout << '\n';
+    cout << "\nData items in original order\n";
+    for(int i = 0; i < 10; i++) cout << arr[i] << " ";
+    cout << "\n\n";
 
     for(int pass = 0; pass < 9; pass++){
-        for(int j = 0; j < 9 - pass; j++){
-            if(arr[j] < arr[j+1]){
+        bool swapped = false; //b
+        for(int j = 0; j < 9 - pass; j++){ //a
+            if(arr[j] > arr[j+1]){ 
                 int tmp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = tmp;
+                swapped = true;
             }
         }
+
+        cout << "After Pass " << (pass + 1) << ": ";
+        for(int i = 0; i < 10; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+
+        if(!swapped) break;
     }
 
-    cout << "\nSorted:\n";
-    for(int i = 0; i < 10; i++){
-        cout << arr[i] << " ";
-    }
-    cout << '\n';
+    cout << "\nData items"<<endl;
+    for(int i = 0; i < 10; i++) cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
