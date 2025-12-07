210d09d2ba04459ed04e34a3fc1c21f402aa1971
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-12-07 17:27:16 +0500

    Task 4

--- PfLab12/Task12.cpp
+++ PfLab12/Task12.cpp
@@ -1,46 +1,31 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"enter size of matrix: ";
-    cin>>n;  
-    int arr[10][10];
-    
-    cout<<"enter elements: "<<endl;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin>>arr[i][j];
+
+int main() {
+    int arr1[25], arr2[5][5];
+    cout << "Enter 25 elements:" << endl;
+    for (int i = 0; i < 25; i++) cin >> arr1[i];
+    int i = 0;
+    int top = 0, bottom = 4, left = 0, right = 4;
+    while (top<=bottom && left<= right) {
+        for (int j=left;j<=right;j++) arr2[top][j] = arr1[i++];
+        top++;
+        for (int j = top; j <= bottom; j++) arr2[j][right] = arr1[i++];
+        right--;
+        if (top <= bottom) {
+            for (int j=right; j>=left; j--) arr2[bottom][j] = arr1[i++];
+            bottom--;
+        }
+        if (left <= right) {
+            for (int j=bottom;j>=top;j--) arr2[j][left] = arr1[i++];
+            left++;
         }
     }
-    
-    bool found = false;
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            bool minRow = true;
-            bool maxCol = true; 
-            for(int k = 0; k < n; k++){
-                if(arr[i][k] < arr[i][j]){
-                    minRow = false;
-                }
-            }
-            
-            for(int k = 0; k < n; k++){
-                if(arr[k][j] > arr[i][j]){
-                    maxCol = false;
-                }
-            }
-            
-            if(minRow && maxCol){
-                cout<<"saddle point at row "<<i+1<<" and column "<<j+1<<" = "<<arr[i][j]<<endl;
-                found = true;
-            }
-        }
+    cout <<endl<<"Spiral form:"<<endl;
+    for (int i2 = 0; i2 < 5; i2++) {
+        for (int j = 0; j < 5; j++) cout << arr2[i2][j] << " ";
+        cout << endl;
     }
-    
-    if(found == false){
-        cout<<"No saddle point"<<endl;
-    }
-    
+
     return 0;
 }
