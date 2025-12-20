764766cd5b9887340d3545b8ac158a0f9b38ee7a
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-12-20 22:17:36 +0500

    Task 4

--- PfLab14/Task14.cpp
+++ PfLab14/Task14.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void reverseUsingPointers(int arr[], int size) {
-    int *start = arr;
-    int *end = arr + size - 1;
-    while (start < end) {
-        int temp = *start;
-        *start = *end;
-        *end = temp;
-        start++;
-        end--;
-    }}
+void print2x3(int arr[2][3]) {
+    int *p = (int*)arr;            
+    int *end = p + (2 * 3);
+    for (; p < end; ++p) {
+        cout << *p << ' ';
+    }
+    cout << '\n';
+}
 
 int main() {
-    const int n = 10;
-    int arr[n];
-    cout << "Enter 10 integers:\n";
-    int *p = arr;
-    int *end = arr + n;
-    for (; p < end; ++p) {
-        cin >> *p;
-    }
-    reverseUsingPointers(arr,n);
-    cout << "After reverse: ";
-    p = arr;
-    for (; p < end; ++p) cout << *p << ' ';
-    cout << '\n';
+    int arr[2][3] = { {1, 2, 3}, {4, 5, 6} };
+    print2x3(arr);
     return 0;
 }
