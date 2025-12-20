4dda807373842b15ac7be3e83547f80c640ad212
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-12-20 21:28:19 +0500

    Task 2

--- PfLab14/Task14.cpp
+++ PfLab14/Task14.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void displayWithPointers(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        int *p = arr + i; 
-        cout <<"Element "<<i<< ": "<<*p<< " AND Address: "<< p << '\n';
+void inputArrayPointer(int arr[], int size) {
+    int *p = arr;
+    int *end = arr + size;
+    for (; p < end; ++p) {
+        cin >> *p;
     }
 }
-int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    displayWithPointers(arr, n);
 
+void printArrayPointer(int arr[], int size) {
+    int *p = arr;
+    int *end = arr + size;
+    for (; p < end; ++p) {
+        cout << *p << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+    int arr[10];
+    int n = 10;
+    cout << "Enter 10 integers:\n";
+    inputArrayPointer(arr, n);
+    cout << "Array contents: ";
+    printArrayPointer(arr, n);
     return 0;
 }
