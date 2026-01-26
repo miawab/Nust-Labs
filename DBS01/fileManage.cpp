e497c172191d4d2c82968e3e8e6c7066eaee9118
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-01-26 15:51:01 +0500

    Task 2

--- DBS01/fileManage.cpp
+++ DBS01/fileManage.cpp
@@ -4,35 +4,69 @@
 using namespace std;
 int main(){
     int i;
-    int size;
-    cout<<"enter array size: ";
-    cin>>size;
+    int intSize;
+    int floatSize;
+    cout<<"enter int array size: ";
+    cin>>intSize;
+    cout<<"enter float array size: ";
+    cin>>floatSize;
 
-    int* arr = new int[size];
+    int* intArr = new int[intSize];
+    float* floatArr = new float[floatSize];
 
-    for(i = 0;i<size;i++){
+    cout<<"int Array:"<<endl;
+    for(i = 0;i<intSize;i++){
         cout<<"enter "<<i+1<<"th item of array: ";
-        cin>>*(arr+i);
+        cin>>*(intArr+i);
+    }
+    cout<<"float Array:"<<endl;
+    for(i = 0;i<floatSize;i++){
+        cout<<"enter "<<i+1<<"th item of array: ";
+        cin>>*(floatArr+i);
     }
 
     ofstream writefile("text.txt");
-    for(i = 0; i<size ;i++){
-        writefile<<*(arr+i)<<endl;
+    writefile<<intSize<<endl;
+    for(i = 0; i<intSize;i++){
+        writefile<<*(intArr+i)<<endl;
+    }
+    writefile<<floatSize<<endl;
+    for(i = 0; i<floatSize;i++){
+        writefile<<*(floatArr+i)<<endl;
     }
     writefile.close();
-    delete[] arr;
+    delete[] intArr;
+    delete[] floatArr;
 
     string item;
 
     ifstream readfile("text.txt");
-    while(1){
+    int integer;
+    float floatnum;
+
+    getline(readfile,item);
+    int size = stoi(item);
+    int* intArr2 = new int[size];
+    for(int i=0;i<size;i++){
         getline(readfile,item);
-        cout<<item<<endl;
-        if(item == ""){
-            break;
-        }
-        item = "";
+        integer = stoi(item);
+        *(intArr2+i)= integer;
+    }
+    cout<<"int Array"<<endl;
+    for(i = 0;i<size;i++){
+        cout<<*(intArr2+i)<<endl;
     }
 
-
+    getline(readfile,item);
+    size = stoi(item);
+    float* floatArr2 = new float[floatSize];
+    for(int i=0;i<size;i++){
+        getline(readfile,item);
+        floatnum = stof(item);
+        *(floatArr2+i)= floatnum;
+    }
+    cout<<"int float"<<endl;
+    for(i = 0;i<size;i++){
+        cout<<*(floatArr2+i)<<endl;
+    }
 }
