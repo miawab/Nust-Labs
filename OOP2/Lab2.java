eb1b042c3535a59f9ca25d422463cbcda0d1fa21
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-01-26 10:47:33 +0500

    Task 5

--- OOP2/Lab2.java
+++ OOP2/Lab2.java
@@ -2,11 +2,7 @@ public class Lab2 {
     public static void main(String[] args)
     {
         int a = 3;
-        int b = 5;
-        System.out.println("a is "+a+" b is "+b);
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println("a is "+a+" b is "+b);
+        int square = a*a;
+        System.out.println("square of "+a+" is "+square);
     }
 }
