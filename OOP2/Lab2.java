04205a643c4822582d073bdef10a5bdc76d20b6e
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-01-26 10:46:04 +0500

    Task 3

--- OOP2/Lab2.java
+++ OOP2/Lab2.java
@@ -1,11 +1,10 @@
 public class Lab2 {
     public static void main(String[] args)
     {
-        int a = 7;
-        int factorial = 1;
-        for(int i=1;i<a+1;i++){
-            factorial = factorial*i;
-        }
-        System.out.println("factorial of "+a+" is "+factorial);
+        int p = 100;
+        int r = 10;
+        int t = 2;
+        int interest = (p*r*t)/100;
+        System.out.println("Interest is "+ interest);
     }
 }
