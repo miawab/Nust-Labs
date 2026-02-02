2d119f40580c6140da4a6829cf63a5f33bd582ea
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-02-02 10:04:37 +0500

    Task 2

--- OOP3/Main.java
+++ OOP3/Main.java
@@ -7,20 +7,14 @@ public class Main {
         System.out.printf("Enter size of array: ");
         int n = sc.nextInt();
         int[] arr1 = new int[n];
+        int count = 0;
         for(int i =0;i<n;i++){
             System.out.println("Enter item: ");
             arr1[i] = sc.nextInt();
+            if(arr1[i]>=10){
+                count++;
+            }
         }
-
-        int temp = arr1[0];
-        arr1[0] = arr1[n-1];
-        arr1[n-1] = temp;
-
-        System.out.println("Array: ");
-
-        for(int i =0;i<n;i++){
-            System.out.print(arr1[i]+"\t");
-        }
-        System.out.println("\n");
+        System.out.println("there are "+count+" integers in this array >= 10");
     }
 }
