20bbe763cbed03b8a84af130d6f25bfdd69f7e20
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-02-09 10:23:15 +0500

    Task3

--- OOP4/Main.java
+++ OOP4/Main.java
@@ -1,22 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-        double tuition = 10000;
-        double rate = 0.06;
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
 
-        for(int i =0;i<10;i++){
-            tuition = tuition + tuition * rate;
+        String topName = "";
+        int topScore = 0;
+
+        for(int i =0;i<n;i++){
+            System.out.print("Enter name: ");
+            String name = sc.next();
+
+            System.out.print("Enter score: ");
+            int score = sc.nextInt();
+
+            if(score > topScore){
+                topScore = score;
+                topName = name;
+            }
         }
 
-        System.out.println("Tuition after 10 years: " + tuition);
-
-        double total = 0;
-
-        for(int i =0;i<4;i++){
-            total = total + tuition;
-            tuition = tuition + tuition * rate;
-        }
-
-        System.out.println("Total cost of 4 years: " + total);
+        System.out.println("Top student: " + topName);
+        System.out.println("Highest score: " + topScore);
     }
 }
