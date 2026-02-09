7f73e9be4a52af705471fcfe468a558ea99d93dd
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-02-09 10:31:11 +0500

    Task6

--- OOP4/Main.java
+++ OOP4/Main.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-        int count = 0;
+        double sum = 0;
+        double[] arr = new double[10];
 
-        for(int i =1;i<=7;i++){
-            for(int j =i+1;j<=7;j++){
-                System.out.println(i + " " + j);
-                count++;
-            }
+        for(int i =0;i<10;i++){
+            System.out.print("Enter number: ");
+            arr[i] = sc.nextDouble();
+            sum = sum + arr[i];
         }
 
-        System.out.println("Total: " + count);
+        double mean = sum / 10;
+        double temp = 0;
+
+        for(int i =0;i<10;i++){
+            temp = temp + Math.pow(arr[i] - mean, 2);
+        }
+
+        double deviation = Math.sqrt(temp / 10);
+
+        System.out.println("Mean: " + mean);
+        System.out.println("Deviation: " + deviation);
     }
 }
